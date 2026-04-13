736b3090647f4c8d63d7f9966039b9cf0635e6bd
pabsgoodev <pabloantoniomascena@gmail.com>
2026-04-12 21:07:45 -0300

    add escapeHtml on controller

--- src/User/userController.ts
+++ src/User/userController.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from 'express';
 import { authService } from './userService';
+import { escapeHtml } from './middlewares/userXss';
 
 export class UserController {
     private service = new authService();
 
     async login(req: Request, res: Response){
-        const { email, password } = req.body;
+        const email = escapeHtml(req.body.email);
+        const password = escapeHtml(req.body.password);
+        
         const service = new authService();
         try{
             const result = await service.exeLogin(email, password);
